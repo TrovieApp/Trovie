a4635983aed4618e87feee7e4d054188ac2eccaf
Kaniyah33 <kaniyah.harris@bison.howard.edu>
2023-05-02 19:45:42 -0400

    Initial Commit

---
+++ Trovie/HomeTableView.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  Trovie
+//
+//  Created by Niyah H on 5/2/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var HomeSearchBar: UISearchBar!
+    
+    @IBOutlet weak var HomeTableView: UITableView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
